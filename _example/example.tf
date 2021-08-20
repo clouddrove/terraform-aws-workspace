b152e40451df96e679382de5cd08adf74dc87216
anmolnagpal <ianmolnagpal@gmail.com>
2021-08-20 23:08:36 +0530

    structurize and update

--- _example/example.tf
+++ _example/example.tf
@@ -4,24 +4,22 @@ provider "aws" {
 }
 module "vpc" {
   source  = "clouddrove/vpc/aws"
-  version = "0.14.0"
+  version = "0.15.0"
 
   name        = "vpc"
   environment = "test"
   label_order = ["name", "environment"]
-  repository  = "https://registry.terraform.io/modules/clouddrove/vpc/aws/0.14.0"
 
   cidr_block = "10.0.0.0/16"
 }
 
 module "subnets" {
   source             = "clouddrove/subnet/aws"
-  version            = "0.14.0"
+  version            = "0.15.0"
   name               = "subnets"
   environment        = "test"
   label_order        = ["name", "environment"]
   availability_zones = ["eu-west-1a", "eu-west-1b"]
-  repository         = "https://registry.terraform.io/modules/clouddrove/subnet/aws/0.14.0"
   vpc_id             = module.vpc.vpc_id
   type               = "public"
   igw_id             = module.vpc.igw_id
@@ -30,14 +28,15 @@ module "subnets" {
 }
 
 module "ad" {
-  source      = "git::https://github.com/clouddrove/terraform-aws-active-directory.git"
-  name        = "ad"
+  source      = "git@github.com:clouddrove/terraform-aws-active-directory.git"
+  name        = "acd"
   environment = "test"
   enabled     = true
 
   subnet_ids                          = module.subnets.public_subnet_id
   vpc_id                              = module.vpc.vpc_id
   ad_name                             = "ad.clouddrove.com"
+  label_order                         = ["name", "environment"]
   ad_password                         = "xyz123@abc"
   ip_whitelist                        = ["51.79.69.69/32", "212.121.146.139/32"]
   enable_internet_access              = true
@@ -52,7 +51,7 @@ module "workspace" {
   enabled     = true
 
   workspace_username = "Administrator"
-  bundle_id          = "wsb-clj85qzj1"
+  label_order        = ["name", "environment"]
+  bundle_id          = "wsb-8pmj7b7pq"
   directory_id       = module.ad.directory_id
-
 }
