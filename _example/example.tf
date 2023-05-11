f00f6fe5c4ab2c446fd697ca9b7b2c9a31ed812c
mamraj yadav <themamraj0131@gmail.com>
2023-05-11 16:21:51 +0530

    bug: updated main.tf and _example and github action

--- _example/example.tf
+++ _example/example.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 module "vpc" {
   source  = "clouddrove/vpc/aws"
-  version = "0.15.1"
+  version = "1.3.0"
 
   name        = "vpc"
   environment = "test"
@@ -15,7 +15,7 @@ module "vpc" {
 
 module "subnets" {
   source             = "clouddrove/subnet/aws"
-  version            = "0.15.3"
+  version            = "1.3.0"
   name               = "subnets"
   environment        = "test"
   label_order        = ["name", "environment"]
